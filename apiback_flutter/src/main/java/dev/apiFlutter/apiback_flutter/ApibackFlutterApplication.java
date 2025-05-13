0774e1a9bf5baed72fe2c2cf74f599e83ceb7f48
KevenRodrigues6868 <kevenlucas2015@gmail.com>
2025-05-13 18:36:41 -0300

    Adicionados arquivos de statement_handle

---
+++ apiback_flutter/src/main/java/dev/apiFlutter/apiback_flutter/ApibackFlutterApplication.java
@@ -0,0 +1,13 @@
+package dev.apiFlutter.apiback_flutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibackFlutterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibackFlutterApplication.class, args);
+	}
+
+}
---
+++ apiback_flutter/src/test/java/dev/apiFlutter/apiback_flutter/ApibackFlutterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.apiFlutter.apiback_flutter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibackFlutterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
